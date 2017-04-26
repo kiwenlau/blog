a68f311b69e331fb30304451650a78e19c8e5383
kiwenlau <kiwenlau@126.com>
2017-04-26 20:23:07 +0800

    popular blogs

--- themes/light/source/js/pv.js
+++ themes/light/source/js/pv.js
@@ -1,9 +1,9 @@
-/*global $, AV, alert, location*/
+/*global $, AV, location*/
 
-function addCount(Counter)
+var Counter = AV.Object.extend("Counter");
+
+function addCount(title, url)
 {
-    var url = $(location).attr("href").trim();
-    var title = $('title').text().trim();
     var query = new AV.Query(Counter);
     query.equalTo("url", url);
     query.find(
@@ -12,99 +12,55 @@ function addCount(Counter)
         {
             if (results.length > 0)
             {
+                // 数据库中已存在该链接时，pv加1
                 var counter = results[0];
-                counter.increment("time");
+                counter.increment("pv");
                 counter.save(null,
                     {
                         fetchWhenSave: true
-                    })
-                    .then(() =>
-                    {
-                        console.log("success");
-                    })
-                    .catch(function(err)
-                    {
-                        console.log(err);
                     });
             }
             else
             {
-                console.log(title);
+                // 数据库中不存在该链接，新建数据
                 var newcounter = new Counter();
                 newcounter.set("title", title);
                 newcounter.set("url", url);
-                newcounter.set("time", 1);
+                newcounter.set("pv", 1);
 
                 newcounter.save(null,
                     {
                         fetchWhenSave: true
-                    })
-                    .then(() =>
-                    {
-                        console.log("success");
-                    })
-                    .catch(function(err)
-                    {
-                        console.log(err);
                     });
-                // newcounter.save(null,
-                // {
-                //     fetchWhenSave: true,
-                //     success: function(newcounter)
-                //     {
-                //         console.log(newcounter);
-                //         //alert('New object created');
-                //     },
-                //     error: function(newcounter, error)
-                //     {
-                //         console.log(err);
-                //         alert('Failed to create');
-                //     }
-                // });
             }
-        },
-        error: function(error)
-        {
-            //find null is not a error
-            console.log(error);
-            alert('Error:' + error.code + " " + error.message);
         }
     });
 }
 
 $(function()
 {
-    var Counter = AV.Object.extend("Counter");
-    //only increse visit counting when intering a page
+    // 获取博客的标题和链接
+    var title = $('header').children("h1.title").text().trim();
+    var url =  $(location).attr("href").trim();
 
-    console.log($('title').text());
+    // 只有博客页面有css样式copyright
+    // 本地访问时，不记录访问量
+    if ($('.copyright').length === 1 && url.indexOf("4000") === -1) addCount(title, url);
 
-    if ($('title').length == 1) addCount(Counter);
     var query = new AV.Query(Counter);
-    query.descending("time");
-    // the sum of popular posts
+    query.descending("pv");
     query.limit(10);
     query.find(
     {
         success: function(results)
         {
-            console.log(results);
             for (var i = 0; i < results.length; i++)
             {
                 var counter = results[i];
                 var title = counter.get("title");
                 var url = counter.get("url");
-                var time = counter.get("time");
-                // add to the popularlist widget
-                var showcontent = title + " (" + time + ")";
-                //notice the "" in href
-
-                $('.popularlist').append('<li><a href="' + url + '">' + showcontent + '</a></li>');
+                $('.popularlist').append('<li><a href="' + url + '">' + title + '</a></li>');
             }
-        },
-        error: function(error)
-        {
-            alert("Error:" + error.code + " " + error.message);
         }
     });
 });
